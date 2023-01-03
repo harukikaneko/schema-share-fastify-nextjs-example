dabe4a6095348badcaf282048262881fad91e494
harukikaneko <niku11298@gmail.com>
2023-01-03 09:07:58 +0900

    fix env name

--- web/src/lib/apiClient.ts
+++ web/src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import aspida from '@aspida/fetch';
 import api from 'api/$api';
 
 const fetchConfig = {
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_URL,
   throwHttpErrors: true, // throw an error on 4xx/5xx, default is false
 };
 
